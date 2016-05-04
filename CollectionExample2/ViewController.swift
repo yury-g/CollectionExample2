fcff7e0f17ca525dc28048778c88951c1d9b4b1a
Voltaire's Ghost <yury@pulsesensor.com>
2016-05-04 13:43:17 -0400

    Added UI Tweak, Icon, and SpeakingEnglish
    Speaks your emoji when selected

--- CollectionExample2/ViewController.swift
+++ CollectionExample2/ViewController.swift
@@ -7,7 +7,14 @@
 //
 
 import UIKit
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+import AVFoundation
+
+
+//  ViewController Class-Wide Variables
+let mySpeechSynth = AVSpeechSynthesizer()
+
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
    
     
     
@@ -26,16 +33,51 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         // Use the outlet in our custom class to get a reference to the UILabel in the cell
         cell1.myLabel.text = items[indexPath.item]
-        cell1.backgroundColor = UIColor.yellowColor() // make cell more visible in our example project
+        cell1.backgroundColor = UIColor.darkGrayColor() // make cell more visible in our example project
+        cell1.layer.borderColor = UIColor.blackColor().CGColor
+        cell1.layer.borderWidth = 1
+        cell1.layer.cornerRadius = 8
         
         return cell1
     }
+    
+    // change background color when user touches cell
+    func collectionView(collectionView: UICollectionView, didHighlightItemAtIndexPath indexPath: NSIndexPath) {
+        let cell1 = collectionView.cellForItemAtIndexPath(indexPath)
+        cell1?.backgroundColor = UIColor.whiteColor()
+    }
+    
+    // change background color back when user releases touch
+    func collectionView(collectionView: UICollectionView, didUnhighlightItemAtIndexPath indexPath: NSIndexPath) {
+        let cell1 = collectionView.cellForItemAtIndexPath(indexPath)
+        cell1?.backgroundColor = UIColor.darkGrayColor()
+    }
+    
     
     // MARK: - UICollectionViewDelegate protocol
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
+        let myInt = indexPath.item
+        print(" that means = \(items[myInt])!")
+        let myEmo = items[myInt]
+        speakThisPhrase(myEmo)
+        
     }
+}
+
+
+
+// from:
+// Pass a String, This Function Speaks it.
+func speakThisPhrase(passedString: String){
+    
+    let myUtterance = AVSpeechUtterance(string: passedString)
+    myUtterance.rate = 0.35
+    myUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+    mySpeechSynth.speakUtterance(myUtterance)
+    
+    
 }
 
