e2cedefde04d846b50d87071f0266add53a61eaa
Voltaire's Ghost <yury@pulsesensor.com>
2016-05-04 12:25:39 -0400

    Emoji Collection View

--- CollectionExample2/ViewController.swift
+++ CollectionExample2/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+   
+    
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // make a cell for each cell index path
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell1 = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell1.myLabel.text = items[indexPath.item]
+        cell1.backgroundColor = UIColor.yellowColor() // make cell more visible in our example project
+        
+        return cell1
     }
-
-
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
 }
 
---
+++ MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//MyCollectionViewCell b//
+//  MyCollectionViewCell.swift
+//  CollectionExample2
+//
+//  Created by Yoda Dev on 5/4/16.
+//  Copyright © 2016 Yury G. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
